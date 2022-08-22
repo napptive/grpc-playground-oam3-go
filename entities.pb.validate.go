cf815bd5817e09322f7b409d84d67775ee9d9197
hermione-napptive <cicd@napptive.com>
2022-08-22 10:53:08 +0000

    gpm automatic publish

--- entities.pb.validate.go
+++ entities.pb.validate.go
@@ -1104,33 +1104,38 @@ func (m *Kube) validate(all bool) error {
 		}
 	}
 
-	if all {
-		switch v := interface{}(m.GetParameters()).(type) {
-		case interface{ ValidateAll() error }:
-			if err := v.ValidateAll(); err != nil {
-				errors = append(errors, KubeValidationError{
-					field:  "Parameters",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
+	for idx, item := range m.GetParameters() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, KubeValidationError{
+						field:  fmt.Sprintf("Parameters[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, KubeValidationError{
+						field:  fmt.Sprintf("Parameters[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
 			}
-		case interface{ Validate() error }:
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
 			if err := v.Validate(); err != nil {
-				errors = append(errors, KubeValidationError{
-					field:  "Parameters",
+				return KubeValidationError{
+					field:  fmt.Sprintf("Parameters[%v]", idx),
 					reason: "embedded message failed validation",
 					cause:  err,
-				})
+				}
 			}
 		}
-	} else if v, ok := interface{}(m.GetParameters()).(interface{ Validate() error }); ok {
-		if err := v.Validate(); err != nil {
-			return KubeValidationError{
-				field:  "Parameters",
-				reason: "embedded message failed validation",
-				cause:  err,
-			}
-		}
+
 	}
 
 	if len(errors) > 0 {
@@ -1208,6 +1213,113 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = KubeValidationError{}
+
+// Validate checks the field values on KubeParameter with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *KubeParameter) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on KubeParameter with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in KubeParameterMultiError, or
+// nil if none found.
+func (m *KubeParameter) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *KubeParameter) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Name
+
+	// no validation rules for Type
+
+	// no validation rules for Required
+
+	// no validation rules for Description
+
+	if len(errors) > 0 {
+		return KubeParameterMultiError(errors)
+	}
+	return nil
+}
+
+// KubeParameterMultiError is an error wrapping multiple validation errors
+// returned by KubeParameter.ValidateAll() if the designated constraints
+// aren't met.
+type KubeParameterMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m KubeParameterMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m KubeParameterMultiError) AllErrors() []error { return m }
+
+// KubeParameterValidationError is the validation error returned by
+// KubeParameter.Validate if the designated constraints aren't met.
+type KubeParameterValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e KubeParameterValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e KubeParameterValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e KubeParameterValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e KubeParameterValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e KubeParameterValidationError) ErrorName() string { return "KubeParameterValidationError" }
+
+// Error satisfies the builtin error interface
+func (e KubeParameterValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sKubeParameter.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = KubeParameterValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = KubeParameterValidationError{}
 
 // Validate checks the field values on CUE with the rules defined in the proto
 // definition for this message. If any rules are violated, the first error
